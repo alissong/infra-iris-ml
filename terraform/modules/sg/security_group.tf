7a63de054845b343775c58178e4c0695f986ae02
Alisson Gomes Souza - Thoughtworks <alisson.souza.tw@naturapay.net>
2025-01-30 17:02:46 -0300

    refactor: update security group ingress rules to allow HTTP and HTTPS traffic

--- terraform/modules/sg/security_group.tf
+++ terraform/modules/sg/security_group.tf
@@ -4,15 +4,31 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "Permitir trafego do ALB para o ECS"
+    description = "Permitir tráfego do ALB para o ECS (porta 8000)"
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
     #security_groups = [aws_security_group.alb_sg.id]
   }
 
+  ingress {
+    description = "Permitir tráfego HTTP (porta 80)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Permitir tráfego HTTPS (porta 443)"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-    description = "Permitir saida para qualquer destino"
+    description = "Permitir saída para qualquer destino"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
