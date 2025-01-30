c0154163669d08e338af69051f65691516ea7b96
Alisson Gomes Souza - Thoughtworks <alisson.souza.tw@naturapay.net>
2025-01-30 17:04:56 -0300

    refactor: update security group ingress descriptions for clarity

--- terraform/modules/sg/security_group.tf
+++ terraform/modules/sg/security_group.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "Permitir tráfego do ALB para o ECS (porta 8000)"
+    description = "Permitir trafego do ALB para o ECS porta 8000"
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
@@ -12,7 +12,7 @@ resource "aws_security_group" "ecs_sg" {
   }
 
   ingress {
-    description = "Permitir tráfego HTTP (porta 80)"
+    description = "Permitir trafego HTTP porta 80"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -20,7 +20,7 @@ resource "aws_security_group" "ecs_sg" {
   }
 
   ingress {
-    description = "Permitir tráfego HTTPS (porta 443)"
+    description = "Permitir trafego HTTPS porta 443"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -28,7 +28,7 @@ resource "aws_security_group" "ecs_sg" {
   }
 
   egress {
-    description = "Permitir saída para qualquer destino"
+    description = "Permitir saida para qualquer destino"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
